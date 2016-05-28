e439db6bcf6020aff0d23929a785a5d9c2887d8a
Mateusz Wieczorek <wskmateusz@gmail.com>
2016-05-28 14:42:57 +0200

    Zmiana sposobu obsługi wyjątków

--- app/src/main/java/dmcs/astroWeather/util/WeatherDownloader.java
+++ app/src/main/java/dmcs/astroWeather/util/WeatherDownloader.java
@@ -2,6 +2,7 @@ package dmcs.astroWeather.util;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,71 +14,59 @@ import java.net.URL;
  */
 public class WeatherDownloader {
 
-    public static String getWoeidByCityAndCountry(String city, String country) {
+    public static String getWoeidByCityAndCountry(String city, String country) throws IOException, JSONException {
         StringBuilder result = new StringBuilder();
         URL url = null;
-        try {
-            url = new URL("https://query.yahooapis.com/v1/public/yql?q=" +
-                    "select%20*%20from%20geo.places(1)%20where%20text=%22" + city +
-                    ",%20" + country + "%22&format=json");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line;
-            while ((line = rd.readLine()) != null) {
-                result.append(line);
-            }
-            rd.close();
-            JSONObject json = new JSONObject(result.toString());
-            return json.getJSONObject("query").getJSONObject("results").getJSONObject("place").getString("woeid");
-        } catch (IOException | JSONException e) {
-            e.printStackTrace();
+        url = new URL("https://query.yahooapis.com/v1/public/yql?q=" +
+                "select%20*%20from%20geo.places(1)%20where%20text=%22" + city +
+                ",%20" + country + "%22&format=json");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
         }
-        return "";
+        rd.close();
+        JSONObject json = new JSONObject(result.toString());
+        return json.getJSONObject("query").getJSONObject("results").getJSONObject("place").getString("woeid");
+
     }
 
-    public static JSONObject getWeatherByWoeid(String woeid) {
+    public static JSONObject getWeatherByWoeid(String woeid) throws IOException, JSONException {
         StringBuilder result = new StringBuilder();
         URL url = null;
-        try {
-            url = new URL("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20" +
-                    "where%20woeid=" + woeid + "%20and%20u=%22c%22&format=json");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line;
-            while ((line = rd.readLine()) != null) {
-                result.append(line);
-            }
-            rd.close();
-            JSONObject json = new JSONObject(result.toString());
-            return json.getJSONObject("query").getJSONObject("results").getJSONObject("channel");
-        } catch (IOException | JSONException e) {
-            e.printStackTrace();
+        url = new URL("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20" +
+                "where%20woeid=" + woeid + "%20and%20u=%22c%22&format=json");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
         }
-        return null;
+        rd.close();
+        JSONObject json = new JSONObject(result.toString());
+        return json.getJSONObject("query").getJSONObject("results").getJSONObject("channel");
+
     }
 
-    public static JSONObject getWeatherByLatitudeAndLongitude(String latitude, String longitude) {
+    public static JSONObject getWeatherByLatitudeAndLongitude(String latitude, String longitude) throws IOException, JSONException {
         StringBuilder result = new StringBuilder();
         URL url = null;
-        try {
-            url = new URL("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20" +
-                    "woeid%20in%20(SELECT%20woeid%20FROM%20geo.places%20WHERE%20text=%22("+latitude+","+longitude+")%22)%20" +
-                    "and%20u=%22c%22&format=json");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line;
-            while ((line = rd.readLine()) != null) {
-                result.append(line);
-            }
-            rd.close();
-            JSONObject json = new JSONObject(result.toString());
-            return json.getJSONObject("query").getJSONObject("results").getJSONObject("channel");
-        } catch (IOException | JSONException e) {
-            e.printStackTrace();
+        url = new URL("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20" +
+                "woeid%20in%20(SELECT%20woeid%20FROM%20geo.places%20WHERE%20text=%22(" + latitude + "," + longitude + ")%22)%20" +
+                "and%20u=%22c%22&format=json");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
         }
-        return null;
+        rd.close();
+        JSONObject json = new JSONObject(result.toString());
+        return json.getJSONObject("query").getJSONObject("results").getJSONObject("channel");
+
     }
 }
--- app/src/test/java/dmcs/astroWeather/util/WeatherDownloaderTest.java
+++ app/src/test/java/dmcs/astroWeather/util/WeatherDownloaderTest.java
@@ -1,7 +1,11 @@
 package dmcs.astroWeather.util;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
+
+import java.io.IOException;
+
 import static junit.framework.Assert.assertTrue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -12,7 +16,7 @@ import static org.hamcrest.core.Is.is;
 public class WeatherDownloaderTest {
 
     @Test
-    public void getWoeid() {
+    public void getWoeid() throws IOException, JSONException {
         String city = "lodz";
         String country = "pl";
         String woeid = WeatherDownloader.getWoeidByCityAndCountry(city, country);
@@ -20,14 +24,14 @@ public class WeatherDownloaderTest {
     }
 
     @Test
-    public void getWeatherByWoeid() {
+    public void getWeatherByWoeid() throws IOException, JSONException {
         String woeid = "505120";
         JSONObject json = WeatherDownloader.getWeatherByWoeid(woeid);
         assertTrue(json != null);
     }
 
     @Test
-    public void getWeatherByLatitudeAndLongitude() {
+    public void getWeatherByLatitudeAndLongitude() throws IOException, JSONException {
         String latitude = "51.756619";
         String longitude = "18.087696";
         JSONObject json = WeatherDownloader.getWeatherByLatitudeAndLongitude(latitude, longitude);
