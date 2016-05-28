c1db68a5dc65ffefad79122283538f5682a056ac
Mateusz Wieczorek <wskmateusz@gmail.com>
2016-05-28 14:38:28 +0200

    Pobieranie pogody

--- app/src/main/java/dmcs/astroWeather/util/WeatherDownloader.java
+++ app/src/main/java/dmcs/astroWeather/util/WeatherDownloader.java
@@ -35,4 +35,49 @@ public class WeatherDownloader {
         }
         return "";
     }
+
+    public static JSONObject getWeatherByWoeid(String woeid) {
+        StringBuilder result = new StringBuilder();
+        URL url = null;
+        try {
+            url = new URL("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20" +
+                    "where%20woeid=" + woeid + "%20and%20u=%22c%22&format=json");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            rd.close();
+            JSONObject json = new JSONObject(result.toString());
+            return json.getJSONObject("query").getJSONObject("results").getJSONObject("channel");
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static JSONObject getWeatherByLatitudeAndLongitude(String latitude, String longitude) {
+        StringBuilder result = new StringBuilder();
+        URL url = null;
+        try {
+            url = new URL("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20" +
+                    "woeid%20in%20(SELECT%20woeid%20FROM%20geo.places%20WHERE%20text=%22("+latitude+","+longitude+")%22)%20" +
+                    "and%20u=%22c%22&format=json");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            rd.close();
+            JSONObject json = new JSONObject(result.toString());
+            return json.getJSONObject("query").getJSONObject("results").getJSONObject("channel");
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
--- app/src/test/java/dmcs/astroWeather/util/WeatherDownloaderTest.java
+++ app/src/test/java/dmcs/astroWeather/util/WeatherDownloaderTest.java
@@ -1,6 +1,8 @@
 package dmcs.astroWeather.util;
 
+import org.json.JSONObject;
 import org.junit.Test;
+import static junit.framework.Assert.assertTrue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -15,6 +17,21 @@ public class WeatherDownloaderTest {
         String country = "pl";
         String woeid = WeatherDownloader.getWoeidByCityAndCountry(city, country);
         assertThat(woeid, is("505120"));
+    }
+
+    @Test
+    public void getWeatherByWoeid() {
+        String woeid = "505120";
+        JSONObject json = WeatherDownloader.getWeatherByWoeid(woeid);
+        assertTrue(json != null);
+    }
+
+    @Test
+    public void getWeatherByLatitudeAndLongitude() {
+        String latitude = "51.756619";
+        String longitude = "18.087696";
+        JSONObject json = WeatherDownloader.getWeatherByLatitudeAndLongitude(latitude, longitude);
+        assertTrue(json != null);
     }
 
 }
